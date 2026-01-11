b949ff1b791e9f41732e4498392d1b1bdfb9dc1f
かがみ <kagami@kagami.me>
2026-01-11 17:25:14 +0800

    feat: refactor countup timer

--- parts/countup-timer.tsx
+++ parts/countup-timer.tsx
@@ -1,103 +1,107 @@
 // similar to CountdownTimer in 'views/components/main/parts/countdown-timer.es', but it counts up
 
-import React, { Component } from 'react'
-import PropTypes from 'prop-types'
+import React, { useState, useEffect, useRef } from 'react'
 
 import { resolveTime } from 'views/utils/tools'
 
-export default class CountupTimer extends Component {
-  static propTypes = {
-    countdownId: PropTypes.string.isRequired, // UNIQUE ID to register to window.ticker
-    startTime: PropTypes.number, // startTime in ms
-    tickCallback: PropTypes.func, // callback function for each second
-    startCallback: PropTypes.func, // callback function when starting to count up
-  }
-  static getTimeElapsed = (startTime, currentTime = Date.now()) => {
-    if (startTime <= 0) {
-      return -1
-    } else if (startTime > currentTime) {
-      return 0
-    }
-    return Math.round((currentTime - startTime) / 1000)
-  }
-  static defaultProps = {
-    startTime: -1,
-    tickCallback: null,
-    startCallback: null,
-  }
-  constructor(props) {
-    super(props)
-    this.timeElapsed = this.constructor.getTimeElapsed(this.props.startTime)
-  }
-  state = {
-    startTime: this.props.startTime,
-  }
-  componentDidMount = () => {
-    this.startTick()
-  }
-  componentWillReceiveProps = (nextProps) => {
-    if (nextProps.countdownId !== this.props.countdownId) {
-      this.stopTick()
-    }
-    if (nextProps.startTime !== this.state.startTime) {
-      this.setState({ startTime: nextProps.startTime })
-      this.timeElapsed = this.constructor.getTimeElapsed(nextProps.startTime)
-    }
-  }
-  shouldComponentUpdate = (nextProps, nextState) =>
-    nextProps.countdownId !== this.props.countdownId ||
-    nextState.startTime !== this.state.startTime
-  componentDidUpdate = () => {
-    this.startTick() // Doesn't matter if it didn't stop
-  }
-  componentWillUnmount = () => {
-    this.stopTick()
-  }
-  startTick = () => {
-    window.ticker.reg(this.props.countdownId, this.tick)
-  }
-  stopTick = () => {
-    window.ticker.unreg(this.props.countdownId)
-  }
-  tick = (currentTime) => {
-    // const actualElapsed = this.constructor.getTimeElapsed(this.state.startTime, currentTime)
-    // if (Math.abs(this.timeElapsed - actualElapsed) > 2) {
-    //   this.timeElapsed = actualElapsed
-    // }
-    this.timeElapsed = this.constructor.getTimeElapsed(
-      this.state.startTime,
-      currentTime,
-    )
-    if (this.timeElapsed < 0) {
-      this.stopTick()
-    }
-    if (this.state.startTime >= 0) {
-      try {
-        if (this.textLabel) {
-          this.textLabel.textContent =
-            resolveTime(this.timeElapsed) || resolveTime(0)
-        }
-        if (this.props.tickCallback) {
-          this.props.tickCallback(this.timeElapsed)
-        }
-        if (this.timeElapsed < 1 && this.props.startCallback) {
-          this.props.startCallback()
-        }
-      } catch (error) {
-        console.error(error.stack)
-      }
-    }
-    this.timeElapsed += 1
-  }
-  render() {
-    return (
-      <span
-        ref={(ref) => {
-          this.textLabel = ref
-        }}
-      >
-        {resolveTime(this.timeElapsed)}
-      </span>
-    )
+interface Ticker {
+  reg: (id: string, callback: (currentTime: number) => void) => void
+  unreg: (id: string) => void
+}
+
+declare global {
+  interface Window {
+    ticker: Ticker
   }
 }
+
+interface CountupTimerProps {
+  countdownId: string
+  startTime?: number
+  tickCallback?: (timeElapsed: number) => void
+  startCallback?: () => void
+}
+
+const getTimeElapsed = (
+  startTime: number,
+  currentTime: number = Date.now(),
+): number => {
+  if (startTime <= 0) {
+    return -1
+  } else if (startTime > currentTime) {
+    return 0
+  }
+  return Math.round((currentTime - startTime) / 1000)
+}
+
+const CountupTimer: React.FC<CountupTimerProps> = ({
+  countdownId,
+  startTime = -1,
+  tickCallback,
+  startCallback,
+}) => {
+  const [displayTime, setDisplayTime] = useState(getTimeElapsed(startTime))
+  const containerRef = useRef<HTMLSpanElement>(null)
+  const isVisibleRef = useRef(true)
+  const startCallbackFiredRef = useRef(false)
+
+  // Set up Intersection Observer to track visibility
+  useEffect(() => {
+    const element = containerRef.current
+    if (!element) return
+
+    const observer = new IntersectionObserver((entries) => {
+      isVisibleRef.current = entries[0].isIntersecting
+    })
+
+    observer.observe(element)
+    return () => observer.disconnect()
+  }, [])
+
+  // Handle ticker registration and prop changes
+  useEffect(() => {
+    const tick = (currentTime: number) => {
+      const timeElapsed = getTimeElapsed(startTime, currentTime)
+
+      if (timeElapsed < 0) {
+        window.ticker.unreg(countdownId)
+        return
+      }
+
+      if (startTime >= 0) {
+        try {
+          if (isVisibleRef.current) {
+            setDisplayTime(timeElapsed)
+          }
+
+          if (tickCallback) {
+            tickCallback(timeElapsed)
+          }
+
+          if (
+            timeElapsed < 1 &&
+            startCallback &&
+            !startCallbackFiredRef.current
+          ) {
+            startCallback()
+            startCallbackFiredRef.current = true
+          }
+        } catch (error) {
+          console.error(error.stack)
+        }
+      }
+    }
+
+    window.ticker.reg(countdownId, tick)
+    setDisplayTime(getTimeElapsed(startTime))
+    startCallbackFiredRef.current = false
+
+    return () => {
+      window.ticker.unreg(countdownId)
+    }
+  }, [countdownId, startTime, tickCallback, startCallback])
+
+  return <span ref={containerRef}>{resolveTime(displayTime)}</span>
+}
+
+export default CountupTimer
